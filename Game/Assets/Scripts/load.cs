2a700657adb161d1fa3ccca3e4a4bff624e76d89
Ayslan Vieira Fontes <78982648+AyslanSE@users.noreply.github.com>
2021-02-20 14:31:02 -0300

    alteração no script de save load

--- Game/Assets/Scripts/load.cs
+++ Game/Assets/Scripts/load.cs
@@ -5,35 +5,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 using System.Runtime.Serialization;
 using System.Xml.Linq;
 using System.Text;
 using System.IO;
 
-[DataContract]public class save1
-{
-    [DataMember]private GameObject _save;
-
-    public GameObject save
-    {
-        get { return _save; }
-        set { _save = value; }
-    }
-    [DataMember]public string scene;
-}
-[Serializable]public class save2
-{
-    [DataMember]private GameObject _save;
-
-    public GameObject save
-    {
-        get { return _save; }
-        set { _save = value; }
-    }
-    [DataMember]public string scene;
-}
-[Serializable]public class save3
+[DataContract]
+public class save1
 {
     [DataMember] private GameObject _save;
 
@@ -44,21 +23,46 @@ using System.IO;
     }
     [DataMember] public string scene;
 }
-[Serializable]public class load : MonoBehaviour
+[DataContract]
+public class save2
 {
-    [SerializeField]private Button loadButton;
+    [DataMember] private GameObject _save;
+
+    public GameObject save
+    {
+        get { return _save; }
+        set { _save = value; }
+    }
+    [DataMember] public string scene;
+}
+[DataContract]
+public class save3
+{
+    [DataMember] private GameObject _save;
+
+    public GameObject save
+    {
+        get { return _save; }
+        set { _save = value; }
+    }
+    [DataMember] public string scene;
+}
+[Serializable]
+public class load : MonoBehaviour
+{
+    [SerializeField] private Button loadButton;
     private string localsave1, localsave2, localsave3;
 
     public GameObject Mysave;
     public string _scene;
     public void Start()
     {
-
-        localsave1 = Application.persistentDataPath + "/save1.dat"; 
+        localsave1 = Application.persistentDataPath + "/save1.dat";
         localsave2 = Application.persistentDataPath + "/save2.dat";
         localsave3 = Application.persistentDataPath + "/save3.dat";
+
         if (File.Exists(localsave1) || File.Exists(localsave2) || File.Exists(localsave3))
-            loadButton.interactable = true; 
+            loadButton.interactable = true;
         else
             loadButton.interactable = false;
     }
@@ -67,16 +71,17 @@ using System.IO;
         if (File.Exists(localsave1) && save == 1)
         {
             FileStream file = File.Open(localsave1, FileMode.Open);
-            BinaryFormatter bf = new BinaryFormatter();
+            XmlSerializer serializer = new XmlSerializer(typeof(save1));
+            save1 data;
 
-            save1 data = (save1)bf.Deserialize(file);
-            Mysave = data.save;
-            _scene = data.scene;
-
-            file.Close();
+            using (Stream reader = new FileStream(localsave1, FileMode.Open))
+            {
+                data = (save1)serializer.Deserialize(reader);
+                Mysave = data.save;
+            }
         }
     }
-    public void save(int save) 
+    public void save(int save)
     {
         if (save == 1)
         {
@@ -88,7 +93,7 @@ using System.IO;
 
             DataContractSerializer bf = new DataContractSerializer(data.GetType());
             MemoryStream streamer = new MemoryStream();
-            
+
             bf.WriteObject(streamer, data);
             streamer.Seek(0, SeekOrigin.Begin);
 
@@ -102,7 +107,7 @@ using System.IO;
         else if (save == 2)
         {
             FileStream file = File.Create(localsave2);
-            save1 data = new save1();
+            save2 data = new save2();
 
             data.save = Mysave;
             data.scene = _scene;
@@ -123,7 +128,7 @@ using System.IO;
         else if (save == 3)
         {
             FileStream file = File.Create(localsave3);
-            save1 data = new save1();
+            save3 data = new save3();
 
             data.save = Mysave;
             data.scene = _scene;
--- Game/Assets/Scripts/menu.cs
+++ Game/Assets/Scripts/menu.cs
@@ -3,11 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    public string sceneselected;
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
-        sceneselected = scene;
     }
     public void OnApplicationQuit()
     {
